c392455e550be8f5469e6903c43d53ec44b6f61f
Alvand Daghoghi <adaghoghi@nyu.edu>
2018-11-20 14:02:53 -0500

    commit

--- HOMEWORK/assignment_10/script.js
+++ HOMEWORK/assignment_10/script.js
@@ -1,5 +1,5 @@
 var cat = document.getElementById("cat");
-var onClick = function(x) {
+function onClick(x) {
 	var hat = document.getElementById("hat"); 
 	hat.style.left = x.clientX - 53 + "px";
 	hat.style.top = x.clientY - 25 + "px";
